1d8f03def21878f3a36fbbef68ba460b6640f4fc
mao <1296193245@qq.com>
2023-11-29 14:54:22 +0800

    uniapp-view-container

---
+++ pages/index/test5/test5.vue
@@ -0,0 +1,52 @@
+<template>
+	<view>
+		<scroll-view refresher-enabled="true" :refresher-triggered="triggered" @refresherrefresh="refresherrefresh()" :scroll-y="true" :lower-threshold="100" @scroll="scroll()" @scrolltolower="lower()">
+			<view v-for="item in list" :key="item">{{item}}</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list:[],
+				triggered:false,
+			}
+		},
+		methods: {
+			lower()
+			{
+				console.log("到底了");
+				const index=this.list.length;
+				for (var i = index; i < index+50; i++) {
+					this.list.push(i)
+				}
+			},
+			scroll(e)
+			{
+				console.log("滚动事件触发");
+			},
+			refresherrefresh()
+			{
+				console.log("下拉刷新被触发");
+				this.list=[];
+				for (var i = 0; i < 50; i++) {
+					this.list.push(i)
+				}
+			}
+		},
+		onLoad() {
+			for (var i = 0; i < 50; i++) {
+				this.list.push(i)
+			}
+		}
+	}
+</script>
+
+<style>
+scroll-view{
+	height: 500px;
+	background-color: antiquewhite;
+}
+</style>
---
+++ pages/index/test7/test7.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<match-media min-width="300" max-width="700">宽度300-700</match-media>
+		<match-media min-width="0" max-width="300">宽度0-300</match-media>
+		<match-media min-width="700" max-width="1000">宽度300-700</match-media>
+		<match-media min-width="1000" max-width="10000">宽度1000-10000</match-media>
+		<match-media min-height="300" max-height="700">高度300-700</match-media>
+		<match-media min-height="0" max-height="300">高度0-300</match-media>
+		<match-media min-height="700" max-height="1000">高度700-1000</match-media>
+		<match-media min-height="1000" max-height="10000">高度1000-10000</match-media>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
