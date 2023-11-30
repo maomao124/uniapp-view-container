529679e3b716a23dd84b94090766dbb5494505af
mao <1296193245@qq.com>
2023-11-30 16:24:28 +0800

    uniapp-view-container

---
+++ pages/index/test8/test8.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<movable-area>
+			<movable-view direction="all" @change="change" :out-of-bounds="true" :inertia="true"></movable-view>
+		</movable-area>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			change(e)
+			{
+				console.log("事件触发,",e);
+			}
+		}
+	}
+</script>
+
+<style>
+movable-area{
+	width: 300px;
+	height: 600px;
+	background-color: aqua;
+}
+movable-view{
+	width: 30px;
+	height: 30px;
+	background-color: red;
+}
+</style>
